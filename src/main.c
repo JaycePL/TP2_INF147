bd22b1c6aaa20cbede816c1c0db3539425d59fc8
cedricbnt <cedricoced@gmail.com>
2024-02-14 18:52:25 -0500

    Main.c prelim

--- src/main.c
+++ src/main.c
@@ -1,3 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void tester_tableau1d(void);
+void tester_tableau2d(void);
+void tester_noyau(void);
+void tetser_image_gris(void);
+
+
 int main() {
-    return 0;
+
+    printf("*********************************************************\n");
+    printf("*                      TEST TABLEAU1D                   *\n");
+    printf("*********************************************************\n");
+    printf("\n");
+    printf("\n");
+
+    printf("*********************************************************\n");
+    printf("*                      TEST TABLEAU2D                   *\n");
+    printf("*********************************************************\n");
+    printf("\n");
+    printf("\n");
+
+    printf("*********************************************************\n");
+    printf("*                        TEST NOYAU                     *\n");
+    printf("*********************************************************\n");
+    printf("\n");
+    printf("\n");
+
+    printf("*********************************************************\n");
+    printf("*                      TEST IMAGE GRIS                  *\n");
+    printf("*********************************************************\n");
+    printf("\n");
+    printf("\n");
+
+
+    return EXIT_SUCCESS;
 }
